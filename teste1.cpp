3fbd5faff569e69107028d36c67df88ae84e2879
Git_1 <vinicius.fs18@gmail.com>
2026-03-09 10:28:17 -0300

    Adicionada exibição de pressão.

--- teste1.cpp
+++ teste1.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main() {
     double temperatura = 26.5;
+    double umidade = 60.0;
+    double pressao = 1013.25;
     cout << "Sensor ativo: " << temperatura << " C" << endl;
+    cout << "Umidade: " << umidade << "%" << endl;
+    cout << "Pressão: " << pressao << " hPa" << endl;
     return 0;
 }
